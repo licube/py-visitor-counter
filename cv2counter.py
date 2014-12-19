27369c1d7eee76ce361db037cf21a1eb53ea0dda
sukso96100 <sukso96100@gmail.com>
2014-12-19 11:06:54 +0900

    update

---
+++ cv2counter.py
@@ -0,0 +1,35 @@
+#-*- coding: utf-8 -*-
+
+__author__ = 'youngbin'
+
+import cv2
+import numpy
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('HaarCascades/face.xml')
+
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+    cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = frame[y:y+h, x:x+w]
+
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+
